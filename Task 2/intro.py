82623455e2bffe704f660f26eca9b03d7f2de2fa
Rishav Raj <srishav1911@gmail.com>
2024-06-22 19:00:47 +0530

    some update

--- Task 2/intro.py
+++ Task 2/intro.py
@@ -4,8 +4,8 @@ import pandas as pd
 # read a csv file from the computer and put it in a dataframe name data
 data = pd.read_csv(r"C:\Users\Rishav Raj\Downloads\netflix_titles.csv")
 
-# show the data
-print(data)
+# show the first 10 data from the dataframe
+print(data.head(10))
 
 # Applying the condition in the release year column, only select the movies or tv shows which are release after year 2015.
 print(data.loc[data['release_year'] > 2015].head(5)) # head(5) represent to showing the top 5 rows of the data frame juts to get the clarity.

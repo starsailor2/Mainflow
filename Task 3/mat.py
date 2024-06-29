f451b3160d3fec38b72db9befdaa418a7d8686ff
Rishav Raj <srishav1911@gmail.com>
2024-06-29 22:20:32 +0530

    draw graph

---
+++ Task 3/mat.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+data = pd.read_csv("C:\Users\Rishav Raj\Downloads\householdtask3.csv")
+
+# To show the result
+plt.show()
+
+
